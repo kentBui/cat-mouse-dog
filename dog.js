e34a0fe681b1ca8748b5bb9a107ea16413996ba8
kent <duccuong.hpe@gmail.com>
2019-12-31 14:26:53 +0700

    add sayHi func

--- dog.js
+++ dog.js
@@ -1,7 +1,11 @@
+var chalk = require('chalk');
 function Dog(){
 	this.stomatch = [];
 }
 Dog.prototype.eat = function(cat){
 	this.stomatch.push(cat);
+};
+Dog.prototype.sayHi = function(){
+	console.log('Hi everyone, i eat'+ chalk.red(' cat!!!!'));
 };
 module.exports = Dog;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var chalk = require('chalk');
 var Dog = require('./dog.js');
 var Cat = require('./cat.js');
 var tom = new Cat('tom',2);
 var milu = new Dog();
 milu.eat(tom);
 console.log(milu);
+milu.sayHi();
